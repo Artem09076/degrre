0950bd835bc3f50d3bedb82fdec586d4afdd11b2
Artem09076 <grigoranartem592@yandex.ru>
2023-10-04 12:14:05 +0300

    add files

--- task2.py
+++ task2.py
@@ -16,13 +16,12 @@ def degree(radius: float, acceleration: float, time: float, velocity: float = 0)
     Returns:
         float: a shift of a dot of the ball, degrees.
     """
-    square = velocity*time + (acceleration*time**2)/2
+    square = velocity * time + (acceleration * time**2) / 2
     area_circus = 360
-    circumference = 2*pi*radius
+    circumference = 2 * pi * radius
     if circumference == 0:
         return 0
-    offset_angle = ceil(area_circus*(square % circumference))
+    offset_angle = ceil(area_circus * (square % circumference))
     if abs(offset_angle) <= area_circus:
         return offset_angle
     return abs(offset_angle) % area_circus
-print(degree(0,2.0,3.0,4.0))
